ccc5f3daa18810967564f32e232daff3384a6cb1
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-25 22:44:20 +0000

    name change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Styles from "../styles/Navbar.module.css"
 
 export default function NavBar() {
     return (<nav className={Styles.nav}>
-        <span>ConfessBin</span>
+        <span>ConfessionBin</span>
 
         <a href="">
         <i class="bi bi-plus-lg"></i>
