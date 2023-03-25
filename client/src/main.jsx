14895f180acac8c39bc82ffa617d90c11d23130c
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-25 16:56:14 +0000

    styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' 
+
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import NavBar from "./components/Navbar"
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
