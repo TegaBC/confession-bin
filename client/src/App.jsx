70be2cafe33063f1d852b87eb2fefe7e766abd0c
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-25 19:33:36 +0000

    added in routing and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom"
+import HomePage from './pages/Home'
 
 function App() {
 
   return (
     <div className="App">
-
+        <Routes>
+          <Route link="/" element={<HomePage/>} />
+        </Routes>
     </div>
   )
 }
