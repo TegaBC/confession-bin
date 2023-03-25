186e54bf11815d817ea9ed1fc426a0ea2f814947
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-25 15:36:46 +0000

    initial server

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+import postRoutes from "./routes/posts.js"
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/ping', (req, res) => {
+    res.send({ message: 'Pong from the server!' });
+});
+
+
+// use a router which has methods like post and get functions and define a main uri route
+app.use("/posts", postRoutes)
+
+// Connect to mongodb
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connected to mongo DB successfully"))
+    .catch(err => console.log(err));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
