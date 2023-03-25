6a0f78912eb89b0f80280033132eab6fed425862
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-25 19:17:11 +0000

    spacing

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -6,12 +6,10 @@ const router = express.Router()
 // create a new router that can be used in the index.js file using the
 // express.use({directory}, router) function
 
-
 /* Define our routes (keep in mind the top level directory)
 if the directory that is stated in the express.use function */
 router.post("/create", createPost)
 router.get("/", getNewPosts)
 router.get("/:id", getPostFromId)
-
 
 export default router
