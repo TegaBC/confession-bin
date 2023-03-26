2ce1a93cde2e6f72eeaf3024fd8777f87c13133a
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-26 18:02:04 +0100

    now renders confessions from the server

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,41 @@
 import Styles from "../styles/Home.module.css"
-
 import Confession from "../components/Confession"
+import { useState } from "react"
+import { useEffect } from "react"
+
+async function fetchNewPosts() {
+    const link = `http://localhost:8080/posts/` // this link gets max 30 new posts
+  
+    try {
+        const response = await fetch(link)
+        const posts = await response.json()
+        console.log("POST: ", posts.message)
+
+        if (response.status == 200) return posts.message
+   } catch(err) {
+        console.log(err)
+   }
+}
 
 export default function HomePage() {
+    const [confessions, setConfessions] =  useState([]) // store current confessions in state
+
+    useEffect(() => { // grab confessions on page loads
+        async function showPosts() {
+            const posts = await fetchNewPosts()
+            setConfessions(posts)
+        }
+
+        showPosts()
+    } , [])
+
     return(
         <div className={Styles.home}>
-            <h1>View the latest confessions</h1>
+            <h1>Latest confessions</h1>
             <h2>Want to make a confession? Click the plus at the top of the screen and post an anonymous confession.</h2>
             <div className={Styles.container}>
-                <Confession title="Hello world" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Odio ullam voluptas accusantium vel libero qui totam ex, architecto quas in molestias quibusdam asperiores 
-                blanditiis minus nemo. Et optio blanditiis voluptatem dignissimos, odit totam explicabo nam illum 
-                cupiditate itaque voluptas nemo ducimus, quibusdam modi sequi cum iure id possimus dolores. Totam."
-                time="10th Dec 2022"/>
-                <Confession title="Hello world" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Odio ullam voluptas accusantium vel libero qui totam ex, architecto quas in molestias quibusdam asperiores 
-                blanditiis minus nemo. Et optio blanditiis voluptatem dignissimos, odit totam explicabo nam illum 
-                cupiditate itaque voluptas nemo ducimus, quibusdam modi sequi cum iure id possimus dolores. Totam."
-                time="10th Dec 2022"/>
-                <Confession title="Hello world" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Odio ullam voluptas accusantium vel libero qui totam ex, architecto quas in molestias quibusdam asperiores 
-                blanditiis minus nemo. Et optio blanditiis voluptatem dignissimos, odit totam explicabo nam illum 
-                cupiditate itaque voluptas nemo ducimus, quibusdam modi sequi cum iure id possimus dolores. Totam."
-                time="10th Dec 2022"/>
+            {confessions.map((post) => <Confession title={post.title} content={post.content} time={post.timestamp} linkId={post._id} />)}
             </div>
-
         </div>
     )
 }
