045c0e6cc3fb489424d725beaa9fbbc0433eea69
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-26 17:03:39 +0100

    logo is now a link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import Styles from "../styles/Navbar.module.css"
+import { Link } from "react-router-dom"
 
 export default function NavBar() {
     return (<nav className={Styles.nav}>
-        <span>ConfessionBin</span>
+        <Link to="/" className={Styles.name}>ConfessionBin</Link>
 
         <a href="">
-        <i class="bi bi-plus-lg"></i>
+        <i className="bi bi-plus-lg"></i>
         </a>
     </nav>)
 }
