40c8dcedc11dbfbf840cb950fc55c64cd8923967
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-26 18:01:36 +0100

    added in an option for it to be clickable

--- client/src/components/Confession.jsx
+++ client/src/components/Confession.jsx
@@ -1,11 +1,15 @@
 import Styles from "../styles/Confession.module.css"
+import { Link } from "react-router-dom"
 
-export default function Confession({ title, content, time }) {
+export default function Confession({ title, content, time, linkId }) {
+    const date = new Date(time)
+    const formattedDate = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short'}).format(date)
+
     return( 
         <div className={Styles.confession}>
-            <h1>{title}</h1>
+            {linkId ? <Link to={`/confession/${linkId}`}>{title}</Link> : <h1>{title}</h1>}
             <p>{content}</p>
-            <h2>{time}</h2>
+            {time && <h2>{formattedDate}</h2>}
         </div>
     )
 }
